82a2814d50ed1b2da3e7b35ca387a787fc136fe8
Chris Kaprys <chriskaprys@gmail.com>
2025-07-17 12:06:25 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch("https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLiQi0w86VNxqZWVhAuU0P45asJvUVvmHAreJpTD5amSRJLOmLXRt4RDJSp2yEE2m6yZXte4tq5PqrwfmDHgO_FuTRlmAXvtMk146Tx9-3yTnslfubhBbDQJE3-GoLMMJWERy5E5WA5Hc0dJP1xNo3YWT81uErXnT__MQxip3r7ItE-rYxD-GOVAWg0UuBxZM7kLLU9TTmDqdbLxLr7MxYKdXlEYW0E7bCi9RgnXT03WXV59oMl-BUItInv4GlDHydiu7rY1Xak-z0OqvVz0EJjnkoqCnA&lib=MJ5QFX8KgkjnPzwXPS3DwKkn2RJ7T_L8O")
+fetch("films.json")
 .then(function(response){
 	return response.json();
 })
