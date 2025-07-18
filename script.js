9d12759971f9662d5f6d8d29a65af4cfd3446f04
Chris Kaprys <chriskaprys@gmail.com>
2025-07-17 23:49:47 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
-fetch("films.json")
-	//just do it
-	//pleeeeeeeeease just refresh!
+fetch("filmlist.json")
 .then(function(response){
 	return response.json();
 })
 .then(function(items){
 	let placeholder = document.querySelector("#data-output");
-	
 	let out = "";
 	for(let results of items.data){
 		// console.log(results.Title);
 		out += `
-			<tr>
-				<td> <img src='${results.Image}'> </td>
-				<td>${results.Title}</td>
-				<td>${results.Year}</td>
-				<td>${results.Genre}</td>
-			</tr>
+			<a target="_blank" rel="noopener noreferrer" href="${results.Link}">
+				<div class="box">
+					<img src="${results.Image}" onerror="this.onerror=null;this.src='image.jpg';">
+					<h4>${results.Title}</h4>
+					<p>${results.Year}</p>
+					<p>${results.Genre}</p>
+				</div>
+			</a>
 		`;
 	};
-
 	placeholder.innerHTML = out;
 });
